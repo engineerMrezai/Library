31c8da4f61df17e05ebaac978846dda285daa879
mahdiRezai <Engineer.m.rezai@gmail.com>
2024-05-12 21:27:41 +0330

    add basic function to lib_main

---
+++ lib_main.py
@@ -0,0 +1,24 @@
+
+
+class Book:
+    def __init__(self, ISBN, Title, Author, Price, Pages):
+        self.ISBN = ISBN
+        self.Title = Title
+        self.Author = Author
+        self.Price = Price
+        self.Pages = Pages
+
+    def add(self):
+        pass
+
+    def delete(self):
+        pass
+
+    def update(self):
+        pass
+
+    def list_book(self):
+        pass
+
+    def search(self):
+        pass
