597af90cee8c0f4038b427f4f07103314fb3d920
mahdiRezai <Engineer.m.rezai@gmail.com>
2024-05-16 16:40:18 +0330

    add basic file for web and create lib_web (index+about)

---
+++ lib_wb.py
@@ -0,0 +1,16 @@
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return flask.render_template("index.html")
+
+
+@app.route('/about')
+def about():
+    return flask.render_template('about.html', greeting="Welcome !")
+
+
+app.run(debug=True)
